74b1135e81c9f4157583424353105184514183ed
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-14 00:36:25 +0800

    initial commit

---
+++ JS/JS_Basics/coding_exercise_18/index.js
@@ -0,0 +1,8 @@
+// String Methods Practice
+
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
+
+let whisper = message.trim().toLowerCase();
