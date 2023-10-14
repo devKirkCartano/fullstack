6ff143eb0d2611e08ab6f72e105792ab09325913
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-14 22:46:51 +0800

    initial commit

---
+++ JS/Array/coding_exercise_26/index.js
@@ -0,0 +1,7 @@
+// Array Access Exercise
+
+const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = "Luna";
+leaderboard[3] = "Draco";
