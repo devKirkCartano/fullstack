fcb4b4c425e3b70db32e410be0e47c28be45b9ab
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-14 13:45:29 +0800

    initial commit

---
+++ JS/JS_Basics/coding_exercise_21/index.js
@@ -0,0 +1,9 @@
+// Our First Conditional Exercise
+
+function isEven(num) {
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+  if (num % 2 === 0) {
+    console.log("even");
+  }
+  //AND THIS LINE ↑↑↑↑↑
+}
