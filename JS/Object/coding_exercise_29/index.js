1ce1597010a617970732bf6896798ab472862797
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-15 15:33:08 +0800

    initial commit

---
+++ JS/Object/coding_exercise_29/index.js
@@ -0,0 +1,10 @@
+// Our First Object Exercise
+
+// Your code goes in here:
+
+const product = {
+  name: "Gummy Bears",
+  inStock: true,
+  price: 1.99,
+  flavors: ["grape", "apple", "cherry"],
+};
