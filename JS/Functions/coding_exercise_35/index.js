065e913d9db4baabaad1ca462aed0dc8d809008c
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-16 22:14:07 +0800

    initial commit

---
+++ JS/Functions/coding_exercise_35/index.js
@@ -0,0 +1,9 @@
+// Heart Function Exercise
+
+// Write your function here:
+
+function printHeart() {
+  console.log("<3");
+}
+
+printHeart();
---
+++ JS/Functions/coding_exercise_36/index.js
@@ -0,0 +1,11 @@
+// Rant Exercise
+
+// DEFINE YOUR FUNCTION:
+
+function rant(message) {
+  console.log(message.toUpperCase());
+  console.log(message.toUpperCase());
+  console.log(message.toUpperCase());
+}
+
+rant("I hate you!");
