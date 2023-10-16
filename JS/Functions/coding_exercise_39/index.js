9d29671d6e53cd595874aeb09a5510136b0a6548
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-10-16 23:49:38 +0800

    initial commit

---
+++ JS/Functions/coding_exercise_39/index.js
@@ -0,0 +1,11 @@
+// isShortsWeather Function
+
+// DEFINE YOUR FUNCTION BELOW:
+
+function isShortsWeather(temperature) {
+  if (temperature >= 75) {
+    return true;
+  } else {
+    return false;
+  }
+}
